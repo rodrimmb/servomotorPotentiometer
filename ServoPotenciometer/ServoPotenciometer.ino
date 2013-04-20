2a1ef110fefdf13562f0b1087c072d14109b9dea
rodrimmbdev <rodrimmb@MacBook-Pro-de-Rodrigo.local>
2013-04-20 16:46:12 +0200

    Llamada a la libreria del servo motor y mapeo de los datos leidos desde el potenciometro

--- ServoPotenciometer/ServoPotenciometer.ino
+++ ServoPotenciometer/ServoPotenciometer.ino
@@ -1,22 +1,32 @@
+#include <Servo.h> 
 
 const int motor = 8;
+int sensorValue;
+
+Servo myservo;  // create servo object to control a servo
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   
-  pinMode(motor,OUTPUT);
+  myservo.attach(motor);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
+  sensorValue = analogRead(A0);
+  // scale it to use it with the servo (value between 0 and 180) 
+  sensorValue = map(sensorValue, 0, 1023, 0, 179);
+  myservo.write(sensorValue);
+  
   // print out the value you read
   Serial.println(sensorValue);
   // delay in between reads for stability
   delay(100);
 }
+
+
 
 
